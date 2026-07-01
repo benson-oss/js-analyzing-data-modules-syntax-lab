d93df06726f67faa8109e1810145f61b26f30195
Your Name <your@email.com>
2026-07-01 15:33:01 +0300

    syntax lab

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 
 
+require("datejs");
+function combineUsers(...args) {
+    const combinedObject = {
+        users: []
+    };
+    for (const usernames of args) {
 
+        combinedObject.users = [
+            ...combinedObject.users,
+            ...usernames
+        ];
+
+    }
+
+    combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+
+    return combinedObject;
+}
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
